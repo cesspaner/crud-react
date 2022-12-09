9c8d8a369d19f2cec4d1dc75e59a5ffb93bfbfe7
FaustoPalacios <78325627+cesspaner@users.noreply.github.com>
2022-12-08 18:43:33 -0600

    listado_en_memoria_cache

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, {useState} from "react"
+import {isEmpty} from 'lodash'
+import shortid from 'shortid'
+
 
 function App() {
+
+  const [ task, setTask] = useState("")
+
+  const [ tasks, setTasks] = useState([])
+
+  const addTask =(e)=>{
+    e.preventDefault()
+    if (isEmpty(task)){
+    console.log(" valido estado no ingreso data")
+    return
+    }
+    const newTask = {
+      id: shortid.generate(),
+      name : task 
+    }
+   
+    setTasks([...tasks, newTask])
+   console.log("ok Ingreso datos")
+   setTask("")
+  }
   return (
   <div className="container mt-5">
     <h1>Registro de actividades</h1>
@@ -10,18 +33,24 @@ function App() {
         <div className="col-8">
          <h4 className="text-center"> Lista de actividades</h4>
           <ul className="list-group"> 
-            <li className="list-group-item">
-            <span className="lead"> Actividades </span>
-            <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-            <button className="btn btn-success btn-sm float-right">Editar</button>
-            </li>
+            {
+              tasks.map((task)=>(
+                <li className="list-group-item" key={task.id}>
+                <span className="lead"> {task.name} </span>
+                <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+                <button className="btn btn-success btn-sm float-right">Editar</button>
+                </li>
+            ))
+            }
           </ul>
         </div>
 
         <div className="col-4">
          <h4 className="text-center">Formulario</h4>
-         <form>
-          <input type="text" className="form-control mb-2" placeholder="Ingrese la tarea...">
+         <form onSubmit={addTask}>
+          <input type="text" className="form-control mb-2" placeholder="Ingrese la tarea..."
+          onChange={(text)=>setTask(text.target.value)}
+          value={task}>
           </input>
           <button className="btn btn-primary btn-block" type="submit">Agregar</button>
          </form>
